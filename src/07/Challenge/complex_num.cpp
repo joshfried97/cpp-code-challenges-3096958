0909c6af21afbae34787b8871d00d98ac1003d35
joshfried97 <joshfried97@gmail.com>
2024-10-21 08:08:19 +0000

    functions added

--- src/07/Challenge/complex_num.cpp
+++ src/07/Challenge/complex_num.cpp
@@ -1,33 +1,55 @@
 #include "complex_num.h"
 
 complex_num::complex_num(double r, double i){
-    // Write your code here
+    real = r;
+    imag = i;
 }
 
 complex_num complex_num::operator +(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real + w.real;
+    z.imag = imag + w.imag;
     return z;
 }
 
 complex_num complex_num::operator -(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real - w.real;
+    z.imag = imag - w.imag;
     return z;
 }
 
 complex_num complex_num::operator *(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real * w.real - imag * w.imag;
+    z.imag = real * w.imag + imag * w.real;
     return z;
 }
 
 complex_num complex_num::operator /(complex_num w){
     complex_num z;
-    // Write your code here
+    double denom = w.real * w.real + w.imag * w.imag;
+    z.real = (real * w.real + imag * w.imag) / denom;
+    z.imag = (real * w.imag + imag * w.real) / denom;
     return z;
 }
 
 void complex_num::print(std::ostream &os){
-    // Write your code here
+    // Determine the sign of the i component before printing
+    std::string op;
+    if (imag < 0)
+    {
+        op = " - ";
+        imag *= -1;
+    } else {
+        op = " + ";
+    }    
+
+    // Now print..
+    if (imag != 0)
+    {
+        os << real << op << "i" << imag;
+    } else {
+        os << real;
+    } 
 }
--- src/07/Challenge/complex_num.h
+++ src/07/Challenge/complex_num.h
@@ -6,6 +6,8 @@
 class complex_num{
     
     // Write your code here
+    double real;
+    double imag; 
 
     public:
          complex_num(double = 0.0, double = 0.0);
